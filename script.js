347269d94b653496734532f19bd146a70b325878
CarlosSalazar34 <carloseliassalazaryunes@gmail.com>
2025-09-18 20:49:23 -0400

    avance-del-proyecto

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hola muchachos :D");
+
+
+const containerSearch = document.querySelector(".search-box");
+const inputSearch = containerSearch.querySelector("input");
+
+inputSearch.addEventListener("focus", () => {
+  inputSearch.style.width = "150px";
+});
+
+inputSearch.addEventListener("blur", () => {
+  inputSearch.style.width = "100px";
+});
